bb20a5f19e24c037ff1c43cd884d3823f723da5a
lily-mieruko-chan <dudapereiraotaku@gmail.com>
2025-09-07 00:17:50 -0300

    Atividade onlien LinearLayout

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,26 +11,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tv;
-    int i =0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        tv = findViewById(R.id.textView);
-        tv.setText(Integer.toString(i));
-        tv.setOnClickListener(v -> {
-            i++;
-            tv.setText((Integer.toString(i)));
 
-                });
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
