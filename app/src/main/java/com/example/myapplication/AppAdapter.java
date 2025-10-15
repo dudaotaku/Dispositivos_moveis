c6fb0884c5e54673d0e7002ed9d1487b11e0af05
dudaotaku <dudaoetaku25@gmail.com>
2025-10-15 19:58:35 -0300

    laucher

---
+++ app/src/main/java/com/example/myapplication/AppAdapter.java
@@ -0,0 +1,52 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class AppAdapter extends ArrayAdapter<ApplicationInfo> {
+
+    Context mContext;
+    int mResourceLayout;
+    public AppAdapter(@NonNull Context context, int resource, @NonNull List<ApplicationInfo> objects) {
+        super(context, resource, objects);
+        this.mContext = context;
+        this.mResourceLayout = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        convertView = inflater.inflate(mResourceLayout,parent,false);
+
+
+        ImageView imageView = convertView.findViewById(R.id.logo);
+        TextView tvAppname = convertView.findViewById(R.id.tvNome);
+
+        ApplicationInfo appInfo = getItem(position);
+
+        if(appInfo == null){
+            return convertView;
+        }
+        imageView.setImageDrawable(appInfo.loadIcon(mContext.getPackageManager()));
+        tvAppname.setText(appInfo.loadLabel(mContext.getPackageManager()).toString());
+
+
+       // imageView.setImageResource(appInfo.logo);
+        //tvAppname.setText(appInfo.loadLabel(mContext.getPackageManager()).toString());
+
+        return convertView;
+       // return super.getView(position, convertView, parent);
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.myapplication;
 
+import android.app.Application;
 import android.app.LocaleManager;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Adapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,19 +15,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    PackageManager pm;
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        LocaleManager lm;
-        getString(R.string.Hello);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        listView = findViewById(R.id.listView);
+
+        pm=getPackageManager();
+        List<ApplicationInfo> apps = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        AppAdapter adapter = new AppAdapter(this,R.layout.app, apps);
+        /*for (ApplicationInfo app: apps){
+            Log.d("App",app.loadLabel(pm).toString());
+        }*/
+        listView.setAdapter(adapter);
     }
 }
