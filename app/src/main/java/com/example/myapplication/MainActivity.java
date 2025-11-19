3f17ce8c45d882bd491e0619b02b28310fda1f93
dudaotaku <dudaotaku25@gmail.com>
2025-11-19 20:52:51 -0300

    gps

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.myapplication;
 
 import android.app.LocaleManager;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.Manifest;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,33 +18,57 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_LOCATION = 1;
+    private static String TAG = "MainActivity";
     LocationManager locationManager;
+    TextView textView;
+    Button bntGetLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        checkAndGetLocation();
+        bntGetLocation = findViewById(R.id.button);
+        bntGetLocation.setOnClickListener(V->checkAndGetLocation());
+
+
     }
 
-    public void getLocation(){
-        requisitandoPermissao();
-        //locationManager.getLastKnownLocation();
+    private void checkAndGetLocation(){
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED &&
-        ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED){
 
             requisitandoPermissao();
             return;
         }
+        getLocation();
+    }
 
+    private void getLocation(){
+        try{
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if(location != null){
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                if(textView != null){
+                    textView.setText("Latitude: " +Double.toString(latitude)+"\nlongitude: "+Double.toString(longitude));
+                }
+            }
+        }catch (SecurityException e){
+
+        }
     }
 
     public void requisitandoPermissao(){
 
-        AppCompatActivity.requestPermissions(this,
-                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION},
+                REQUEST_LOCATION
         );
 
     }
